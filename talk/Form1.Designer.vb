02c9c227b83450eec91661872b19930e9469a9be
Daxesh512 <99954315+Daxesh512@users.noreply.github.com>
2025-11-20 02:01:01 +0530

    Media player

---
+++ talk/Form1.Designer.vb
@@ -0,0 +1,156 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.btnFastReverse = New System.Windows.Forms.Button()
+        Me.btnFastForward = New System.Windows.Forms.Button()
+        Me.btnStop = New System.Windows.Forms.Button()
+        Me.btnPause = New System.Windows.Forms.Button()
+        Me.btnPlay = New System.Windows.Forms.Button()
+        Me.btnBrowse = New System.Windows.Forms.Button()
+        Me.AxWindowsMediaPlayer1 = New AxWMPLib.AxWindowsMediaPlayer()
+        Me.OpenFileDialog1 = New System.Windows.Forms.OpenFileDialog()
+        CType(Me.AxWindowsMediaPlayer1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(0, 3)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(69, 13)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "Now Playing:"
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Location = New System.Drawing.Point(67, 0)
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.ReadOnly = True
+        Me.TextBox1.Size = New System.Drawing.Size(261, 20)
+        Me.TextBox1.TabIndex = 1
+        '
+        'btnFastReverse
+        '
+        Me.btnFastReverse.Location = New System.Drawing.Point(260, 200)
+        Me.btnFastReverse.Name = "btnFastReverse"
+        Me.btnFastReverse.Size = New System.Drawing.Size(68, 29)
+        Me.btnFastReverse.TabIndex = 13
+        Me.btnFastReverse.Text = "|<|"
+        Me.btnFastReverse.UseVisualStyleBackColor = True
+        '
+        'btnFastForward
+        '
+        Me.btnFastForward.Location = New System.Drawing.Point(260, 168)
+        Me.btnFastForward.Name = "btnFastForward"
+        Me.btnFastForward.Size = New System.Drawing.Size(68, 29)
+        Me.btnFastForward.TabIndex = 12
+        Me.btnFastForward.Text = "|>|"
+        Me.btnFastForward.UseVisualStyleBackColor = True
+        '
+        'btnStop
+        '
+        Me.btnStop.Location = New System.Drawing.Point(260, 133)
+        Me.btnStop.Name = "btnStop"
+        Me.btnStop.Size = New System.Drawing.Size(68, 29)
+        Me.btnStop.TabIndex = 11
+        Me.btnStop.Text = "Stop"
+        Me.btnStop.UseVisualStyleBackColor = True
+        '
+        'btnPause
+        '
+        Me.btnPause.Location = New System.Drawing.Point(260, 98)
+        Me.btnPause.Name = "btnPause"
+        Me.btnPause.Size = New System.Drawing.Size(68, 29)
+        Me.btnPause.TabIndex = 10
+        Me.btnPause.Text = "Pause"
+        Me.btnPause.UseVisualStyleBackColor = True
+        '
+        'btnPlay
+        '
+        Me.btnPlay.Location = New System.Drawing.Point(260, 63)
+        Me.btnPlay.Name = "btnPlay"
+        Me.btnPlay.Size = New System.Drawing.Size(68, 29)
+        Me.btnPlay.TabIndex = 9
+        Me.btnPlay.Text = "Play"
+        Me.btnPlay.UseVisualStyleBackColor = True
+        '
+        'btnBrowse
+        '
+        Me.btnBrowse.Location = New System.Drawing.Point(260, 28)
+        Me.btnBrowse.Name = "btnBrowse"
+        Me.btnBrowse.Size = New System.Drawing.Size(68, 29)
+        Me.btnBrowse.TabIndex = 8
+        Me.btnBrowse.Text = "Browse..."
+        Me.btnBrowse.UseVisualStyleBackColor = True
+        '
+        'AxWindowsMediaPlayer1
+        '
+        Me.AxWindowsMediaPlayer1.Enabled = True
+        Me.AxWindowsMediaPlayer1.Location = New System.Drawing.Point(3, 26)
+        Me.AxWindowsMediaPlayer1.Name = "AxWindowsMediaPlayer1"
+        Me.AxWindowsMediaPlayer1.OcxState = CType(resources.GetObject("AxWindowsMediaPlayer1.OcxState"), System.Windows.Forms.AxHost.State)
+        Me.AxWindowsMediaPlayer1.Size = New System.Drawing.Size(251, 206)
+        Me.AxWindowsMediaPlayer1.TabIndex = 7
+        '
+        'OpenFileDialog1
+        '
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(335, 242)
+        Me.Controls.Add(Me.btnFastReverse)
+        Me.Controls.Add(Me.btnFastForward)
+        Me.Controls.Add(Me.btnStop)
+        Me.Controls.Add(Me.btnPause)
+        Me.Controls.Add(Me.btnPlay)
+        Me.Controls.Add(Me.btnBrowse)
+        Me.Controls.Add(Me.AxWindowsMediaPlayer1)
+        Me.Controls.Add(Me.TextBox1)
+        Me.Controls.Add(Me.Label1)
+        Me.Name = "Form1"
+        Me.ShowIcon = False
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "Form1"
+        CType(Me.AxWindowsMediaPlayer1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents TextBox1 As System.Windows.Forms.TextBox
+    Friend WithEvents btnFastReverse As System.Windows.Forms.Button
+    Friend WithEvents btnFastForward As System.Windows.Forms.Button
+    Friend WithEvents btnStop As System.Windows.Forms.Button
+    Friend WithEvents btnPause As System.Windows.Forms.Button
+    Friend WithEvents btnPlay As System.Windows.Forms.Button
+    Friend WithEvents btnBrowse As System.Windows.Forms.Button
+    Friend WithEvents AxWindowsMediaPlayer1 As AxWMPLib.AxWindowsMediaPlayer
+    Friend WithEvents OpenFileDialog1 As System.Windows.Forms.OpenFileDialog
+
+End Class
---
+++ talk/Form1.vb
@@ -0,0 +1,51 @@
+﻿
+Public Class Form1
+
+
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Me.FormBorderStyle = Windows.Forms.FormBorderStyle.FixedSingle
+        Me.Text = "Wisdom Player"
+        Me.MaximizeBox = False
+    End Sub
+
+    Private Sub btnBrowse_Click_1(sender As Object, e As EventArgs) Handles btnBrowse.Click
+        OpenFileDialog1.Title = "Selecting File to play..."
+        OpenFileDialog1.InitialDirectory = System.Environment.SpecialFolder.Desktop
+        If (OpenFileDialog1.ShowDialog = DialogResult.OK) Then
+            AxWindowsMediaPlayer1.URL = OpenFileDialog1.FileName
+        End If
+    End Sub
+
+    Private Sub btnPlay_Click_1(sender As Object, e As EventArgs) Handles btnPlay.Click
+        AxWindowsMediaPlayer1.Ctlcontrols.play()
+    End Sub
+
+    Private Sub btnPause_Click_1(sender As Object, e As EventArgs) Handles btnPause.Click
+        AxWindowsMediaPlayer1.Ctlcontrols.pause()
+    End Sub
+
+    Private Sub btnStop_Click_1(sender As Object, e As EventArgs) Handles btnStop.Click
+        AxWindowsMediaPlayer1.Ctlcontrols.stop()
+    End Sub
+
+    Private Sub btnFastForward_Click_1(sender As Object, e As EventArgs) Handles btnFastForward.Click
+        AxWindowsMediaPlayer1.Ctlcontrols.fastForward()
+    End Sub
+
+    Private Sub btnFastReverse_Click(sender As Object, e As EventArgs) Handles btnFastReverse.Click
+        AxWindowsMediaPlayer1.Ctlcontrols.fastReverse()
+    End Sub
+
+    Private Sub OpenFileDialog1_FileOk(sender As Object, e As System.ComponentModel.CancelEventArgs) Handles OpenFileDialog1.FileOk
+        Try
+            If OpenFileDialog1.FileName <> Nothing Or OpenFileDialog1.FileName <> "" Then
+                TextBox1.Text = (OpenFileDialog1.FileName.Substring(OpenFileDialog1.FileName.LastIndexOf("\") + 1, (OpenFileDialog1.FileName.IndexOf(".", 0) - (OpenFileDialog1.FileName.LastIndexOf("\") + 1))))
+            End If
+        Catch ex As Exception
+            TextBox1.Text = "UnKnown Title"
+        End Try
+    End Sub
+
+    
+End Class
